ac512cb929472e33447f90963641c2979ce017fd
awprevite <awprevite@gmail.com>
2025-06-05 12:22:54 -0400

    Initial commit

---
+++ Chip Count/Chip_CountApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chip_CountApp.swift
+//  Chip Count
+//
+//  Created by Anthony Previte on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Chip_CountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chip CountTests/Chip_CountTests.swift
@@ -0,0 +1,17 @@
+//
+//  Chip_CountTests.swift
+//  Chip CountTests
+//
+//  Created by Anthony Previte on 6/5/25.
+//
+
+import Testing
+@testable import Chip_Count
+
+struct Chip_CountTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
